d007c4d38cb1bb6de0c0963b97640b1641eeb749
chromalence <nathanielotremba@gmail.com>
2026-04-05 22:16:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,36 +5,7 @@ BIN_PATH="/usr/local/bin"
 MAN_PATH="/usr/local/man/man7"
 TIMER=5
 
-
-check_deps() { # its big but expandable
-  local missing=()
-  for tool in elfkickers gcc sstrip; do # linux comes with gcc so it shouldnt be an issue
-    if ! command -v "$tool" &> /dev/null; then
-      missing+=("$tool")
-    fi
-  done
-
-  if [ ${#missing[@]} -ne 0 ]; then
-    echo "-- missing dependencies: ${missing[*]} --"
-    
-    if command -v pacman &> /dev/null; then
-      sudo pacman -S "${missing[@]}"
-    elif command -v apt &> /dev/null; then
-      sudo apt install "${missing[@]/nvim/neovim}" 
-    elif command -v dnf &> /dev/null; then
-      sudo dnf install "${missing[@]}"
-    elif command -v xbps-install &> /dev/null; then
-      sudo xbps-install "${missing[@]}"
-    elif command -v zypper &> /dev/null; then
-      sudo zypper install "${missing[@]}"
-    elif command -v emerge &> /dev/null; then
-      sudo emerge "${missing[@]}"
-    fi
-    
-  else
-    echo "all dependencies found congrats"
-  fi
-}
+# do the dependency check yourself its safer anyway hope you understand
 
 echo "installing zcore.."
 sudo mkdir -p "$LIB_PATH/zcore" &&
@@ -43,8 +14,8 @@ echo "installing zcore man 7.." &&
 sudo cp doc/zcore.7 "$MAN_PATH/zcore.7" &&
 echo "zcore finished installing into $LIB_PATH/zcore"
 echo "tbt installing in $TIMER/s ( <CTRL+C> to abort nothing will break )"
+echo "make sure you have: elfkickers / elf-kickers & a C compiler"
 sleep 5 &&
-check_deps &&
 sudo mkdir -p "$BIN_PATH" &&
 sudo cp -r extra/tbt "$BIN_PATH" &&
 echo "tbt finished installing" &&
